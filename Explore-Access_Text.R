706979e4ba0aafe99b8aadcee59bab4e567f612c
Adrienne <adrienne@axiomdatascience.com>
2021-06-18 14:54:10 -0800

    starting out with an R script

---
+++ Explore-Access_Text.R
@@ -0,0 +1,14 @@
+#Using R to do Shell Data curation
+#Adrienne trying to at least
+#Last updated: June 2021
+#git repo for folder with script, etc
+#data too big, stored in neighboring directory for access
+#using gitrepo https://github.com/AdrienneCanino/R-over-shell-drives.git
+
+# Load up data wrangling environment
+#I always want tidyverse? Yea, I always want tidyverse.
+library("tidyverse")
+install.packages('tidyverse')
+
+#get into the data, look at the text files
+#
