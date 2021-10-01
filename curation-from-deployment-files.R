eb4b018d95a699006d1109f44c91e7687f994473
Adrienne <adrienne@axiomdatascience.com>
2021-10-01 15:21:23 -0800

    loop to build single Df from deploymentinfo csvs - until I realized it's the wrong values being row-bound :(

--- curation-from-deployment-files.R
+++ curation-from-deployment-files.R
@@ -54,59 +54,46 @@ deployCol <- as.character(c("recorderId", "recorderMake","recorderVersion","stat
 drive81Deploys <- read.table("~/Documents/R-over-shell-drives/CSV-copied/deploymentInfo-OV2013.csv", 
                              header=F, sep="," ,
                              blank.lines.skip = FALSE, comment.char="")
+
+#The problem had been using 'fill=T' in read.table(), it was making the number of cols misread.
+#the comment character was '#', so it was reading lines 1 AND 6 as comments, thus leaving them out of the final dataframe. #tricky tricky
 #How can I get all the csvs into one table?
 #if it's all in the working directory
-
+dat =NULL
 deployDF <- NULL
 setwd("~/Documents/R-over-shell-drives/CSV-copied/")
 files <- list.files("~/Documents/R-over-shell-drives/CSV-copied/", pattern="*.csv")
 for (f in files){
-  dat <- read.table(f, header=F, sep=",",blank.lines.skip = FALSE, comment.char="") #read table
-  dat <- dat[-c(1:6),-1] #drop row weirdness
+  dat <- read.table(f, skip=6, 
+                    header=F, sep="," ,
+                    blank.lines.skip = FALSE, comment.char="", colClasses = c("character","character", "character")) #read table
   #seems like, there's not a good way right away to preserve the 3 values in that row weirdness
-  print(dat)
-  colnames(dat, deployCol)
-  deployDF <- rbind(deployDF, dat)
-}
-#Error: number of coluymns of arguments do not match..... that's odd?
-#whoa, OW09 does have significantly fewer columns. Is that going to happen often?
-#trying again, error: Error in is.data.frame(x) && do.NULL : invalid 'y' type in 'x && y', 
+  head(dat)
+  deployDF <- dplyr::bind_rows(deployDF, dat)
+} #loop finally working!
+#but now I need to add to it.
+view(deployDF)
 
-#that didn't work, it's missing the key few rows that are the best functional header in this spreadsheet
-#even with options maxed out, lines 1 and 6 in the spreadsheet aren't translating into the dataframe
+#this loop worked once, but it didn't continue the loop?
+#I'm going to look at each deployment df seperately
+files
+drive81Deploy2013 <- read.table("~/Documents/R-over-shell-drives/CSV-copied/deploymentInfo-OV2013.csv", 
+                             header=F, sep="," ,
+                             blank.lines.skip = FALSE, comment.char="")
 
-#The problem had been using 'fill=T' in read.table(), it was making the number of cols misread.
-#the comment character was '#', so it was reading lines 1 AND 6 as comments, thus leaving them out of the final dataframe. #tricky tricky
-#still not wroking well?
+drive81Deploy2009 <- read.table("~/Documents/R-over-shell-drives/CSV-copied/deploymentInfo-OW09.csv", 
+                             header=F, sep="," ,
+                             blank.lines.skip = FALSE, comment.char="")
+
+drive81Deploy2014 <- read.table("~/Documents/R-over-shell-drives/CSV-copied/deploymentInfo-SU14.csv", 
+                                header=F, sep="," ,
+                                blank.lines.skip = FALSE, comment.char="", skip=5)
 
 
-#in Python:
-#drive81-deploy = read_csv()
 
+view(drive81Deploy2014) #this one is loading in with more rows than appropriate ? Fixed now.
 
-#                            # look through the text inventory for each drive for wav files
-#                            # grep sometimes seems to return duplicates though, so the second part is to try and address that. I think it would work.
-#                            $ grep ".wav" drive.inv.txt | sort -u > drive.wavs
-#                            
-#                            # for each wav file found, add a row to the wav table
-#                            
-# assumes a structure like 'deploymentInfo.csv' on ax70 
-#                            # also, this is a a slightly different approach than what I descried on the call 
-#                            for wav in drive.wavs:
-#                              recorder_id = find code between penultimate and last "/"
-#                            station_id = find value between penultimate and antepenultimate "/"
-#                            open drive.deploymentInfo.csv as drive.df
-#                            copy row where station_id == drive.df[stationId] AND recorder_id == drive.df[recorderId]
-#                            append row to drive.deploy
-#                            drive.df.loc[new row, 'file path'] = wav
-#                            drive.df.loc[row, 'file name'] = wav[find(last "/")+1:] to 
-#                            
-#                            # I think this should give us a dataframe with all that contextual info for each wav file from each deployment. 
-#                            # That might not be the best way to go though. Maybe we do want to keep the authoritative deployment doc(s) separate from the wav inventory. 
-#                            
-
-#OK, I think I get it,but I also think it's not going to work consistently because the subdirectories are in fact a hot mess in ax81.
-#but something very near it could work, using the value for recorder_id to search the text file
+#the subdirectories are in fact a hot mess in ax81.
 
 #OK. So, what I want now, is, a spreadsheet that uses the recorderID and station ID from deploy df, to find the .wav file in wavsdf, and with the matching, make a curation df that lists the info from the relevant row in deploy df (will repeat alot), the wav file path, and file name, and still need to do, calculated values in there like total volume, or just maybe, size of that file, 
 
@@ -116,7 +103,7 @@ curatedf = data.frame()
 
 #find an exact match of instance in wavs file list, of recorderID and station ID from deployment df
 
-recorderID <- clean81deploy$recorderId[1]
+recorderID <- deployDF$recorderId
 stationID <-  clean81deploy$stationId[1]
 recorderID
 
