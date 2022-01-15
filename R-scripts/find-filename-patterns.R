56752d068fe94ead0372c81ccb05ed609e9143f3
Adrienne <adrienne.canino@gmail.com>
2022-01-14 17:57:13 -0900

    starting to find files by filename patterned, instead of .wav ending

---
+++ R-scripts/find-filename-patterns.R
@@ -0,0 +1,13 @@
+##Using R to do Shell Data curation -----
+#Adrienne trying to at least
+#Last updated: January 2022
+#git repo for folder with script, etc
+#new.invs too big, stored in neighboring directory for access
+#using gitrepo https://github.com/AdrienneCanino/R-over-shell-drives.git
+
+#This do it all take two is about, getting to a more streamlined process, while also testing the process on a drive other than 81. let's do, 72?
+
+#install.packages('tidyverse')
+library("tidyverse")
+library("stringi")
+
